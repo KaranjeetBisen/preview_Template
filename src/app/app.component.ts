6d0d4860a62072305aed004d3ea7b0a24f95dcc5
Karanjeet Bisen <karanjeetbisen@gmail.com>
2025-01-26 10:55:25 +0530

    updated routes and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { EditorComponent } from "./components/editor/editor.component";
 import { HttpClientModule } from '@angular/common/http';
 import { PreviewComponent } from './components/preview/preview.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [EditorComponent,HttpClientModule, PreviewComponent],
+  imports: [HttpClientModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+import { EditorComponent } from './components/editor/editor.component';
 
 export const routes: Routes = [
 
+    {path:'', component:BaseComponent,
+        children:[
+            {path:'preview' , component: EditorComponent }
+        ]
+    }
 ];
